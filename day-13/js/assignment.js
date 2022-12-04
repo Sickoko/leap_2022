8262a16838bf3e5278975fa0dc1adbde698edf1c
Sickoko <Usukhuumz12@gmail.com>
2022-12-04 23:23:38 +0800

    worked a little more

--- day-13/js/assignment.js
+++ day-13/js/assignment.js
@@ -2,15 +2,24 @@ console.log(Math.floor(Math.random() * 100));
 //(max - min + 1)) + min
 console.log(Math.floor(Math.random() * (255 - 50 + 1) + 50 ));
 
-let max = Number(prompt("give a number"));
-let count = 1;
+// let max = Number(prompt("give a number"));
+// let count = 1;
 
-let sum = 0;
+// let sum = 0;
 
-while (count <= max) {
-  count = count + 1;
-  sum = sum + count;
-  console.log(count);
-  console.log(sum);
-}
+// while (count <= max) {
+//   count = count + 1;
+//   sum = sum + count;
+//   console.log(count);
+//   console.log(sum);
+// }
+
+// let hello = 'Hello I am Usukhbayar. \n ';
+// let age = 'I am 18 years old. \n ';
+// let newLine = '   This is the new line with tab and some \ backslashes \.'
+// alert(hello + age + newLine)
+
+
+const firstName = Usukhbayar;
+const lastName = Tumenulzii;
 
